26245bf06ffce6aafa2455f1004a4e37a1ba2e18
Ayushya Shah <corphaxnation@gmail.com>
2026-08-10 16:09:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'haxnation-cache-v4';
+const CACHE_NAME = 'haxnation-cache-v5';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -9,6 +9,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
@@ -37,6 +38,6 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
-    ))
+    )).then(() => self.clients.claim())
   );
 });
